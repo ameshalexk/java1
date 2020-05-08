06673ae1c83e288de6d10fc5ef167a3d67646cd9
ameshalexk <ameshalexusa@gmail.com>
2020-05-08 01:29:31 -0400

    jsobjlearning

---
+++ js_objects.js
@@ -0,0 +1,23 @@
+//var students = [];
+
+function Student(first,last,age) {
+    this.firstName = first;
+    this.lastName = last;
+    this.age = age;
+    this.greeting = function() {
+        return "Hi, I am " + this.firstName + " and I am " + this.age + "years old.";
+    };
+}
+
+students.push(new Student("Jenny","Laga",5));
+students.push(new Student("Timmy","Turner",8));
+students.push(new Student("Carl","Jr",4));
+
+for ( let index = 0; index < students.length; index++) {
+    student = students[index];
+   console.log(student.greeting());
+}
+
+//console.log(students)
+
+
